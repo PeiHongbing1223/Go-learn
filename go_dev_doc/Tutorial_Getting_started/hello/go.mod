33a7d84045ddf4817a0115813a3be4587d4b31a2
PeiHongbing1223 <peihongb@dingtalk.com>
2024-02-06 21:52:45 +0800

    update

--- go_dev_doc/Tutorial_Getting_started/hello/go.mod
+++ go_dev_doc/Tutorial_Getting_started/hello/go.mod
@@ -1,3 +1,5 @@
 module github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/hello
 
 go 1.21.0
+
+require github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings v0.0.0-20240205115616-7f77848e89e1
--- go_dev_doc/Tutorial_Getting_started/hello/hello.go
+++ go_dev_doc/Tutorial_Getting_started/hello/hello.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "log"
 
-	"github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings"
+    "github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
-}
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(messa
