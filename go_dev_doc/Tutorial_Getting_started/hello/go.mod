6bb29b18600550ac9070795cc68ae0ab8ffd79e9
PeiHongbing1223 <peihongb@dingtalk.com>
2024-02-09 13:48:17 +0800

    update

--- go_dev_doc/Tutorial_Getting_started/hello/go.mod
+++ go_dev_doc/Tutorial_Getting_started/hello/go.mod
@@ -2,4 +2,4 @@ module github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/h
 
 go 1.21.0
 
-require github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings v0.0.0-20240205115616-7f77848e89e1
+require github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings v0.0.0-20240206135245-33a7d84045dd
--- go_dev_doc/Tutorial_Getting_started/hello/hello.go
+++ go_dev_doc/Tutorial_Getting_started/hello/hello.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings"
+	"github.com/PeiHongbing1223/Go-learn/go_dev_doc/Tutorial_Getting_started/greetings"
 )
 
 func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("")
-    // If an error was returned, print it to the console and
-    // exit the program.
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(messa
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+}
